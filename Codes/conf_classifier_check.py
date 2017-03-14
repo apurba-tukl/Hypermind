2b4379aaa1ae657968e8292bca0a27efc9a076df
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2017-03-14 23:50:13 +0100

    StandardScaler

--- Codes/conf_classifier_check.py
+++ Codes/conf_classifier_check.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import os
@@ -22,6 +23,10 @@ for p in range(1, 15):
     X_test = testing.drop(["participant", "answer", "confidence"], axis=1, inplace=False)
     y_test = testing["confidence"]
     labels.extend(y_test)
+
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
 
     clf = SVC(class_weight="balanced")
     clf.fit(X_train, y_train)
